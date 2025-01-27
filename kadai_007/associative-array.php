d70d4f3d21a08d16235d49a4d3506f59ddce137c
poterika <tennissssr@gmail.com>
2025-01-28 00:48:21 +0900

    kadai_007を追加

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+       <?php
+       // 連想配列を作成し、変数に代入
+       $tamanegi=['name'=>'onion', 'price'=>'200', 'weight'=>'160'];
+
+       //値を出力する
+       print_r($tamanegi);
+       ?>
+   </p>
+</body>
+
+</html>
+    
+
+
